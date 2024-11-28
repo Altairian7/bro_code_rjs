09e39e29f8995352018c52e27003fb67e9a5e50a
Altairian7 <artisharma.as12.as@gmail.com>
2024-11-28 20:23:54 +0530

    simple card fliping divs added to xyz.jsx page

---
+++ src/xyz.jsx
@@ -0,0 +1,41 @@
+import "./Xyz.css";
+
+export default function Xyz() {
+  return (
+    <>
+      <div class="card">
+        <div class="card-inner">
+          <div class="card-front">
+            <h1>How</h1>
+          </div>
+          <div class="card-back">
+            <h1>I'm Fine</h1>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-inner">
+          <div class="card-front">
+            <h1>Are</h1>
+          </div>
+          <div class="card-back">
+            <h1>Thank You</h1>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-inner">
+          <div class="card-front">
+            <h1>You?</h1>
+          </div>
+          <div class="card-back">
+            <h1>Adios!</h1>
+          </div>
+        </div>
+      </div>
+
+
+      
+    </>
+  );
+}
