5652fc37c0cd470d94b8a9ce5f8600f4917a3911
Altairian7 <artisharma.as12.as@gmail.com>
2024-12-13 22:54:00 +0530

    added food to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer.jsx";
+import Food from "./Components/Food.jsx";
 
 function App() {
 
   return (
     <>
       <Header />
+      <Food />
       <Footer />
-      {/* <Food /> */}
     </>
   );
 }
---
+++ src/Components/Food.jsx
@@ -0,0 +1,15 @@
+    
+function Food() {
+    const food_1 = "Orange";
+    const food_2 = "Apple";
+
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food_1}</li>
+            <li>{food_2}</li>
+        </ul>
+    )
+}
+
+export default Food
