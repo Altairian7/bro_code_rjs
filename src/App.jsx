9fa7aefdcbcc06e831aa02ada316bbd075f41522
Altairian7 <artisharma.as12.as@gmail.com>
2024-12-13 22:34:08 +0530

    added footer to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer.jsx";
 
 function App() {
 
   return (
     <>
       <Header />
-      {/* <Footer /> */}
+      <Footer />
       {/* <Food /> */}
     </>
   );
